fe7303e00e8da040c8f9fc92aaa7fc5714d3fb10
Sameer Kushwaha <sameerkushwaha2003@gmail.com>
2024-07-05 19:02:40 +0530

    Second Commit

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 from flask import Flask , render_template , request
 app = Flask(__name__)
 
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World!'
+
+# @app.route('/home')
+# def home():
+#     return render_template("home.html")
+
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-@app.route('/home')
-def home():
-    return render_template("home.html")
-
-@app.route('/form')
 def form():
     return render_template("form.html")
 
